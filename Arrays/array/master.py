123925cc1e1f8f33f458ff53d90597e5ac3fe1e9
Mohamed Mehdawy <mohammedmehdawy@gmail.com>
2022-08-07 15:34:40 +0200

    build append method in array

--- Arrays/array/master.py
+++ Arrays/array/master.py
@@ -2,8 +2,8 @@ import ctypes
 
 class Array:
     def __init__(self, size):
-        self.array_of_types = ctypes.py_object * size
-        self.memory = self.array_of_types()
+        self.array_data_type = ctypes.py_object * size
+        self.memory = self.array_data_type()
         self.size = size
         
         for i in range(size):
@@ -17,3 +17,24 @@ class Array:
     
     def __setitem__(self, idx, value):
         self.memory[idx] = value
+    def append(self, item):
+        # create new array
+        array_data_type = ctypes.py_object * (self.size + 1)
+        new_memory = array_data_type()
+        
+        # copy old array
+        for i in range(self.size):
+            new_memory[i] = self.memory[i]
+            
+        # put new element
+        new_memory[self.size] = item
+        self.size += 1
+        
+        # del old array
+        del self.memory
+        self.memory = new_memory
+
+arr = Array(1)
+arr[0] = "ahmed"
+arr.append(20)
+print(arr[1])
