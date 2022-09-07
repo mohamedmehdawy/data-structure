15bf90dda3f771cf2b170ce0efc5b2df70d9cdd0
Mohamed Mehdawy <mohammedmehdawy@gmail.com>
2022-09-07 20:18:30 +0200

    start Problem #1: Insert front

---
+++ Singly-Linked-List/Homework/SLL Homework 1/SLL/master.py
@@ -0,0 +1,148 @@
+class Node:
+    def __init__(self, value, next=None):
+        self.data = value
+        self.next = next
+
+    def __repr__(self) -> str:
+        return f"{self.data}"
+    
+
+class LinkedList:
+    def __init__(self, init_values=None):
+        self.head = None
+        self.tail = None
+        self.length = 0
+        self._debug_data = []
+        if init_values: 
+            for value in init_values:
+                self.insert_end(value)
+
+    def insert_end(self, value):
+        node = Node(value)
+        if self.head == None:
+            self.head = self.tail = node
+        else:
+            self.tail.next = node
+            self.tail = node
+        self.length += 1
+        self._debug_data.append(node)
+
+    def get_nth(self, n):
+        if n <= self.length:
+            current = self.head
+            for _ in range(n - 1):
+                current = current.next
+            return current
+        return None
+
+    def index(self, value):
+        temp_head = self.head
+        idx = 0
+        while temp_head:
+            if temp_head.data == value:
+                return idx
+            temp_head = temp_head.next
+            idx += 1
+        return -1
+    
+    def index_transposition(self, value):
+        temp_head = self.head
+        prev = None
+        idx = 0
+        while temp_head:
+            if temp_head.data == value:
+                if prev:
+                    temp_head.data, prev.data = prev.data, temp_head.data
+                    return idx - 1
+                return idx
+            prev = temp_head
+            temp_head = temp_head.next
+            idx += 1
+        return -1
+
+    def _debug_verify_data_integrity(self):
+        if self.length == 0:
+            assert self.head == None
+            assert self.tail == None
+            return
+        
+        assert self.head != None
+        assert self.tail != None
+        assert self.tail.next == None
+        
+        if self.length == 1:
+            assert self.head == self.tail
+        elif self.length == 2:
+            assert self.head.next == self.tail
+        else:
+            temp_head = self.head
+            counter = 0
+            
+            while temp_head:
+                temp_head = temp_head.next
+                counter += 1
+                assert counter < 1000
+                
+            assert self.length == counter
+            assert self.length == len(self._debug_data)
+        
+        return "all is good"
+    def _debug_print_exsiting_nodes(self):
+        result = ""
+        temp_head = self.head
+        while temp_head:
+            result += f"{temp_head}\t->\t{temp_head.next}"
+            if temp_head == self.head:
+                result += "\t head"
+            elif temp_head.next == None:
+                result += "\t tail"
+            result += "\n"
+            temp_head = temp_head.next
+
+        result += "*" * 30
+        return result
+    
+    def _debug_print_address(self):
+        result = ""
+        temp_head = self.head
+        while temp_head:
+            result += f"{id(temp_head)}\t->\t{id(temp_head.next) if temp_head.next else temp_head.next}"
+            if temp_head == self.head:
+                result += "\t head"
+            elif temp_head.next == None:
+                result += "\t tail"
+            result += "\n"
+            temp_head = temp_head.next
+
+        result += "*" * 30
+        return result
+    def __repr__(self) -> str:
+        result = "["
+        temp_head = self.head
+        for i in range(self.length):
+            result += f"{temp_head}{',' if i < self.length - 1 else ''}"
+            temp_head = temp_head.next
+        result += "]"
+        return result
+
+    def __len__(self):
+        return self.length
+
+    def __iter__(self):
+        current = self.head
+        while current:
+            yield current
+            current = current.next
+        
+
+ll = LinkedList([1, 2])
+print(ll._debug_print_exsiting_nodes())
+print(ll._debug_print_address())
+# for value in [15, 15, 15, 15, 15]:
+#     print(f"index of {value} => {ll.index_transposition(value)}")
+#     print(ll)
+# for value in [8, 6, 99]:
+#     print(f"index of {value} => {ll.index_transposition(value)}")
+#     print(ll)
+
+print(ll._debug_verify_data_integrity())
