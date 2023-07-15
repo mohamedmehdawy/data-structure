400fe627586b19be9c358d29cbc7982d8a67b4ec
Mohamed Mehdawy <mohammedmehdawy@gmail.com>
2023-07-15 16:39:28 +0300

    found the problem should solve it

--- Singly-Linked-List/Homework/SLL-Homework-4/tasks/master.py
+++ Singly-Linked-List/Homework/SLL-Homework-4/tasks/master.py
@@ -657,6 +657,10 @@ class LinkedList:
                 # get chain_tail, chain_head, next_chain_head
                 chain_tail, chain_head, next_chain_head = _reverse_subchain(next_chain_head, k)
                 
+                # fix it by disconnect each chain and fix chain_head, next_chain_head
+                print(f"chain tail: {chain_tail}")
+                print(f"chain head: {chain_head}")
+                print(f"next chain head: {next_chain_head}")
                 # if this first chain
                 if not self.head:
                     self.head = chain_head
