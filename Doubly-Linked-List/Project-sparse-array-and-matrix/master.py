4555cdd830f87eaa25cf0eafd10851c9721b0ac6
Mohamed Mehdawy <mohammedmehdawy@gmail.com>
2023-10-18 00:47:01 +0300

    make set value in test more flexiable

--- Doubly-Linked-List/Project-sparse-array-and-matrix/master.py
+++ Doubly-Linked-List/Project-sparse-array-and-matrix/master.py
@@ -41,13 +41,13 @@ class SparseMatrix:
         results = []
         
         while current_row:
-            results.append(f"ROW {current_row.idx}: {current_row.data}")
+            results.append(f"Row {current_row.idx}: {current_row.data}")
             current_row = current_row.next
         
         return "\n".join(results)
 
 
-def test1(row, column, expected):
+def test1(data, row, column, expected):
     func_name = inspect.currentframe().f_code.co_name
     
     print(f"testing => set value")
@@ -55,15 +55,15 @@ def test1(row, column, expected):
     ll = SparseMatrix(row, column)
     
 
-    ll.set_value(1,1,5)
-    ll.set_value(1,2,10)
-
-    print(f"the value is: {ll}")
+    for ele in data:
+        for row in range(1, len(ele)):
+            ll.set_value(ele[0], ele[row][0], ele[row][1])
+    
     assert str(ll) == expected, f"Mismatch between expected={expected}, and result={ll} in {func_name}"
 
 
 
 if __name__ == "__main__":
-    test1(7,7, "ROW 1: 5@1, 10@2")
-    
+    test1([[1,[1,5], [2,10]], [3, [1,50]]],7,7, "Row 1: 5@1, 10@2\nRow 3: 50@1")
+    test1([[1, [6,-3]], [5, [2,6], [5,1]], [2, [5,8]], [3, [0,9], [3,7], [5,4]], [6, [6,-8]]], 7, 7, "Row 1: -3@6\nRow 2: 8@5\nRow 3: 9@0, 7@3, 4@5\nRow 5: 6@2, 1@5\nRow 6: -8@6")
     print("all tests passed")
