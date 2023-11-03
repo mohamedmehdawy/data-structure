c3c1af5188013179dace08c065d5a68cfb294263
Mohamed Mehdawy <mohammedmehdawy@gmail.com>
2023-11-04 01:00:59 +0200

    add ADT for stack

---
+++ 7-stack/ArrayStack/master.py
@@ -0,0 +1,22 @@
+class Stack:
+    
+    def __init__(self) -> None:
+        pass
+    
+    def push(self, value):
+        pass
+    
+    def pop(self):
+        pass
+    
+    def peek(self):
+        pass
+    
+    def isEmpty(self):
+        pass
+    
+    def isFull(self):
+        pass
+    
+    def clearContent(self):
+        pass
