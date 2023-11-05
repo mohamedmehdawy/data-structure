30b83f669b6fe18f0b065cf8e1528d15d3e31b1f
Mohamed Mehdawy <mohammedmehdawy@gmail.com>
2023-11-05 19:48:09 +0200

    build pop function and tests passed

--- 7-stack/ArrayStack/master.py
+++ 7-stack/ArrayStack/master.py
@@ -8,13 +8,21 @@ class Stack:
     
     def push(self, value):
         """
+            Time Complexity: O(1)
+            Memory Complexity: O(1)
+            ########################
             this function push the value to the stack
         """
         self.array.append(value)
     
     def pop(self):
-        pass
-    
+        """
+            this function remove the last item from the array
+        """
+        if len(self.array):
+            return self.array.pop()
+
+        return None
     def peek(self):
         pass
     
@@ -51,8 +59,37 @@ def test1(data, expected):
     assert result == expected, f"Mismatch between expected={expected}, and result={result} in {func_name}"
     
     
+def test2(data, pop_count,expected):
+    func_name = inspect.currentframe().f_code.co_name
+    
+    print(f"testing => pop")
+    
+    # stack
+    stk = Stack()
+    
+    # push data
+    for ele in data:
+        stk.push(ele)
+        
+    # pop the last element
+    for _ in range(pop_count):
+        stk.pop()
+        
+    # result
+    result = str(stk)
+    
+    assert result == expected, f"Mismatch between expected={expected}, and result={result} in {func_name}"
+    
 if __name__ == "__main__":
+    # test 1 => push
     test1([], "")
     test1([1], "1")
+    test1([1,2,3,4,5], "1, 2, 3, 4, 5")
     
+    # test 2 => pop
+    test2([], 1, "")
+    test2([1], 1, "")
+    test2([1,2,3,4,5], 1, "1, 2, 3, 4")
+    test2([1,2,3,4,5], 2, "1, 2, 3")
+
     print("all tests passed")
