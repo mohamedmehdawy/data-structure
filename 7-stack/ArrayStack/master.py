2a6e16e9a44fb40fa73e090d71256b04f1f95a4b
Mohamed Mehdawy <mohammedmehdawy@gmail.com>
2023-11-05 18:33:08 +0200

    fix repr return

--- 7-stack/ArrayStack/master.py
+++ 7-stack/ArrayStack/master.py
@@ -1,10 +1,16 @@
+import inspect
+
+
 class Stack:
     
     def __init__(self) -> None:
-        pass
+        self.array = []
     
     def push(self, value):
-        pass
+        """
+            this function push the value to the stack
+        """
+        self.array.append(value)
     
     def pop(self):
         pass
@@ -20,3 +26,33 @@ class Stack:
     
     def clearContent(self):
         pass
+    
+    def __repr__(self) -> str:
+        
+        # convert each element in array to string to convert the array to a one string
+        return ", ".join(str(item) for item in self.array)
+        
+
+def test1(data, expected):
+    func_name = inspect.currentframe().f_code.co_name
+    
+    print(f"testing => push")
+    
+    # stack
+    stk = Stack()
+    
+    # push data
+    for ele in data:
+        stk.push(ele)
+        
+    # result
+    result = str(stk)
+    
+    assert result == expected, f"Mismatch between expected={expected}, and result={result} in {func_name}"
+    
+    
+if __name__ == "__main__":
+    test1([], "")
+    test1([1], "1")
+    
+    print("all tests passed")
