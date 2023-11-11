7b33258c6836e58df7e3e259ee4391d865a91579
Mohamed Mehdawy <mohammedmehdawy@gmail.com>
2023-11-11 20:47:04 +0200

    solve reverse number

--- 7-stack/HomeWorks/HomeWork-1/reverse-number/master.py
+++ 7-stack/HomeWorks/HomeWork-1/reverse-number/master.py
@@ -1,3 +1,6 @@
+import inspect
+
+
 def reverse_number(num):
     """
         this function take the int number and reverse it and store in a stack
@@ -19,17 +22,33 @@ def reverse_number(num):
     while num > 0:
         current_number = num % 10
         current_list.append(current_number)
-        num /= 10
+        num //= 10
         
     # create the stack
-    stack = current_list.copy().reverse()
+    stack = current_list.copy()
+    stack.reverse()
     
     # check trailing zeros
     while stack[len(stack) - 1] == 0:
         stack.pop()
-        
     # return the stack
-    return "".join(stack)
+    return "".join(str(stack[index]) for index in range(len(stack) - 1, -1, -1))
 
-# def test1(data, expected):
+def test1(num, expected):
+    fun_name = inspect.currentframe().f_code.co_name
     
+    print(f"{fun_name} => reverse_number")
+    
+    result = reverse_number(num)
+    
+    assert result == expected, f"Mismatch between expected={expected}, and result={result} in {fun_name}"
+    
+
+if __name__ == "__main__":
+    
+    # test 1 => reverse_number
+    test1(12345000, "54321")
+    test1(5102030, "302015")
+    
+    # all tests passed
+    print("all tests passed")
