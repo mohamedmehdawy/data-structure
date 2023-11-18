436ab92c9c48c7d622a47af90e1869de3c53f0bd
Mohamed Mehdawy <mohammedmehdawy@gmail.com>
2023-11-18 21:36:26 +0200

    solve with O(n^2), but have time limit error

--- 7-stack/HomeWorks/HomeWork-2/Daily Temperatures/master.py
+++ 7-stack/HomeWorks/HomeWork-2/Daily Temperatures/master.py
@@ -4,4 +4,31 @@ class Solution(object):
         :type temperatures: List[int]
         :rtype: List[int]
         """
+        # check if empty
+        if not temperatures:
+            return []
         
+        # check if have 1 element
+        if len(temperatures) == 1:
+            return [0]
+        
+        # create answers and stack
+        answers = []
+        
+        for i in range(len(temperatures)):
+            status = False
+            for j in range(i+1, len(temperatures)):
+                if(temperatures[i] < temperatures[j]):
+                    status = True
+                    break
+            
+            # check status, if true apppend the difference if not, append 0
+            if status:
+                answers.append(j - i)
+            else:
+                answers.append(0)
+                
+        return answers
+    
+    
+print(Solution().dailyTemperatures([89,62,70,58,47,47,46,76,100,70]))
