390ba78a3e9c82742fa04ff2818b6395a6210deb
Mohamed Mehdawy <mohammedmehdawy@gmail.com>
2023-11-19 07:15:09 +0200

    solve with class style

--- 7-stack/HomeWorks/HomeWork-2/Problem #1 Manual System Stack/master.py
+++ 7-stack/HomeWorks/HomeWork-2/Problem #1 Manual System Stack/master.py
@@ -36,7 +36,40 @@ def f_stk(n):
         
     return result
 
-
+def f_stk_class_style(n):
+    if n <= 1:
+        return 5
+    
+    class RecursiveCall:
+        def __init__(self,n, result = 0, is_completed=False) -> None:
+            self.n = n
+            self.result = result
+            self.is_completed = is_completed
+            
+    stack = [RecursiveCall(n)]
+    
+    while stack:
+        # get last call data
+        last_call = stack[-1]
+        n, result, is_completed = last_call.n, last_call.result, last_call.is_completed
+        
+        if not is_completed:
+            
+            # check last call
+            if n <= 1:
+                stack.append(RecursiveCall(n, 5, True))
+            elif n % 3 == 0:
+                stack.append(RecursiveCall(n-1-n%3, 6))
+            else:
+                stack.append(RecursiveCall(n-1-n%2, 8))
+        else:
+            stack.pop()
+            
+            if not stack:
+                return result
+            
+            stack[-1].result += result
+            stack[-1].is_completed = True
 def test1(n):
     fun_name = inspect.currentframe().f_code.co_name
     
@@ -44,6 +77,21 @@ def test1(n):
     
     try:
         f_stk_result = f_stk(n)
+        f_result = f(n)
+        
+        print(f"f result: {f_result}, stk result: {f_stk_result}")
+        assert f_result == f_stk_result
+        
+    except:
+        print(f"stk result: {f_stk_result}")
+        
+def test2(n):
+    fun_name = inspect.currentframe().f_code.co_name
+    
+    print(f"testing: {fun_name} -> simulate stack class style")
+    
+    try:
+        f_stk_result = f_stk_class_style(n)
         f_result = f(n)
         
         print(f"f result: {f_result}, stk result: {f_stk_result}")
@@ -59,6 +107,11 @@ if __name__ == "__main__":
     test1(10)
     test1(500)
     test1(100000)
+    
+    # test 2
+    test2(10)
+    test2(500)
+    test2(100000)
 
     
     
