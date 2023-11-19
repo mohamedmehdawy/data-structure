a0a2ee44092c84bcc507ef5aa05270c2c7f6ce8d
Mohamed Mehdawy <mohammedmehdawy@gmail.com>
2023-11-19 12:35:28 +0200

    improve Daily Temperatures

--- 7-stack/HomeWorks/HomeWork-2/Daily Temperatures/master.py
+++ 7-stack/HomeWorks/HomeWork-2/Daily Temperatures/master.py
@@ -13,26 +13,13 @@ class Solution(object):
             return [0]
         
         # create answers and stack
-        answers = []
+        answers = [0] * len(temperatures)
         stack = []
         
-        for index in range(1, len(temperatures)):
-            # append answer
-            answers.append(0)
+        for index, temperature in enumerate(temperatures):
             
-            # check if the current element is less than the next one  1
-            if temperatures[index-1] < temperatures[index]:
-                answers[index - 1] = 1
-                
-            else:
-                stack.append(index-1)
-                
-            # check stack
-            while stack and temperatures[stack[-1]] < temperatures[index]:
-                answers[stack[-1]] = index - stack[-1]
-                stack.pop()
-                
-                
-        # append 0 for last element
-        answers.append(0)      
+            while stack and temperatures[stack[-1]] < temperature:
+                prev_index = stack.pop()
+                answers[prev_index] = index - prev_index
+            stack.append(index)
         return answers              
