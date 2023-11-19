8703c5eebf3fab20401f2930fbd0bbdfc5d5c13f
Mohamed Mehdawy <mohammedmehdawy@gmail.com>
2023-11-19 08:03:17 +0200

    improve Score of Parentheses

--- 7-stack/HomeWorks/HomeWork-2/Score of Parentheses/master.py
+++ 7-stack/HomeWorks/HomeWork-2/Score of Parentheses/master.py
@@ -10,30 +10,16 @@ class Solution(object):
             return 0
         
         # create stack and scrore
-        # stack will have [Boolean: for multi by 2, value: the value of the paranthese]
-        stack = []
-        score = 0
-        
+        # stack will start with init state to store the scrore
+        stack = [0]
+
         for char in s:
             if char == "(":
-                if stack and not stack[-1][0]:
-                    last_element = stack[-1]
-                    last_element[0] = True
-                    last_element[1] = 0
-                stack.append([False, 1])
-
+                stack.append(0)
             else:
-                last_element = stack[-1]
-                if not last_element[0] and len(stack) > 1:
-                    stack[-2][1] += last_element[1]
-                elif last_element[0] and len(stack) > 1:
-                    stack[-2][1] += last_element[1] * 2
-                else:
-                    if last_element[0]:
-                        
-                        score += last_element[1] * 2
-                    else:
-                        score += last_element[1]
-                stack.pop()
-                    
-        return score
+                # if is 0, this is not a parent
+                value = max(2 * stack.pop(), 1)
+                
+                stack[-1] += value
+                
+        return stack[-1]
