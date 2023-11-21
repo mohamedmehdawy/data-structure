d548d4a8c1369cd97ec92f8206860d1776dde2a1
Mohamed Mehdawy <mohammedmehdawy@gmail.com>
2023-11-21 08:13:33 +0200

    add precedence function

--- 7-stack/07 infixToPostfix/code/master.py
+++ 7-stack/07 infixToPostfix/code/master.py
@@ -9,15 +9,20 @@ def infix_to_postfix(infix):
         returns:
             postfix after conversion
     """
-    # set order for each operator
-    operators_order = {
-        '#': 0,
-        '+': 1,
-        '-': 1,
-        '*': 2,
-        '/': 2
-    }
-    
+    def precedence(operator):
+        """
+            this function return the precedence of the operator
+            parameters:
+                operator: the operator will get precedence of it
+            returns:
+                the precedence of the operator
+        """
+        if operator == "+" or operator == "-":
+            return 1
+        elif operator == "*" or operator == "/":
+            return 2
+
+        return 0
     
     # create postfix list and operators stack
     postfix_list = []
@@ -31,12 +36,12 @@ def infix_to_postfix(infix):
     
     for token in infix:
         # if not operator append to postfix list
-        if token not in operators_order:
+        if token.isdigit():
             postfix_list.append(token)
         else:
 
                 # pop operators from stack and append to list until the condition return false
-                while not(operators_order[operators_stack[-1]] < operators_order[token]):
+                while not(precedence(operators_stack[-1]) < precedence(token)):
                     postfix_list.append(operators_stack.pop())
                 # if stack is empty or the current token is more than the last element in stack
                 # or iteration end, append operator
