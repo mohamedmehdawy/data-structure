1c14c6c0a76c32a8fcc403ceeb642fa119cd9fe5
Mohamed Mehdawy <mohammedmehdawy@gmail.com>
2023-11-22 07:01:00 +0200

    handle parantheses when convert

--- 7-stack/07 infixToPostfix/code/master.py
+++ 7-stack/07 infixToPostfix/code/master.py
@@ -22,6 +22,7 @@ def infix_to_postfix(infix):
         elif operator == "*" or operator == "/":
             return 2
 
+
         return 0
     
     # create postfix list and operators stack
@@ -39,13 +40,21 @@ def infix_to_postfix(infix):
         if token.isdigit():
             postfix_list.append(token)
         else:
-
-                # pop operators from stack and append to list until the condition return false
-                while not(precedence(operators_stack[-1]) < precedence(token)):
-                    postfix_list.append(operators_stack.pop())
-                # if stack is empty or the current token is more than the last element in stack
-                # or iteration end, append operator
-                operators_stack.append(token)
+                # if the operator is ) pop last element until arraive to (
+                if token == ")":
+                    
+                    while operators_stack[-1] != '(':
+                        postfix_list.append(operators_stack.pop())
+                    
+                    # remove (
+                    operators_stack.pop()
+                else:
+                    # pop operators from stack and append to list until the condition return false
+                    while token != '(' and operators_stack[-1] != '(' and not(precedence(operators_stack[-1]) < precedence(token)):
+                        postfix_list.append(operators_stack.pop())
+                    # if stack is empty or the current token is more than the last element in stack
+                    # or iteration end, append operator
+                    operators_stack.append(token)
                 
     # convert list to string and return the value
     return "".join(postfix_list)
@@ -63,6 +72,10 @@ if __name__ == "__main__":
     # test1 => infix to posfix
     test1("1+3*5-8/2", "135*+82/-")
     test1("1+2*3", "123*+")
+    test1("2+3*4-5*6+7", "234*+56*-7+")
+    test1("(1+2)*3", "12+3*")
+
+    test1("2+(3*(4-5*2)*(9/3+6))", "23452*-*93/6+*+")
     
     # all tests passed
     print("all tests passed")
