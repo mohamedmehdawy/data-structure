1097562a656cada705f6e17ce179e4071fb2024b
Mohamed Mehdawy <mohammedmehdawy@gmail.com>
2023-11-26 12:24:31 +0200

    init evalaute_postfix

---
+++ 7-stack/HomeWorks/Homework-3/Postfix Evaluation/master.py
@@ -0,0 +1,9 @@
+def evalaute_postfix(postfix):
+    """
+        this function evalaute postfix and return the value
+        parameters:
+            postfix: the postfix value will converted
+        returns:
+            postfix value after evalaute
+    """
+    # check if less than 3 element, return the postfix
