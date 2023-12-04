33c68b82e19bc8100979727cbd9d40e7cedb1aff
Mohamed Mehdawy <mohammedmehdawy@gmail.com>
2023-12-04 15:57:35 +0200

    ADT for the functoin

---
+++ 7-stack/HomeWorks/Homework-4/Remove Expression Brackets/master.py
@@ -0,0 +1,9 @@
+def remove_brackets(expression):
+    """
+        this function remove all brackets from the expression and simplify it
+        parameters:
+            expression: the expression will remove () from it
+        return:
+            the expression after remove () and simplify it
+    """
+    pass
