a248856df7742f0a8331f927f18fed547b7561d8
Mohamed Mehdawy <mohammedmehdawy@gmail.com>
2023-12-13 07:45:25 +0200

    handle front and rear with prev and next functions, and start test enque front function

--- Queue/Homeworks/homework-1/deque/master.py
+++ Queue/Homeworks/homework-1/deque/master.py
@@ -3,20 +3,30 @@ from packages.doubleLinkedList import DoubleLinkedList
 class Deque:
     # init properties
     def __init__(self, size) -> None:
+        self.front = self.rear = 0
         self.size = size
         self.added_elements = 0
         self.double_linked_list = DoubleLinkedList()
-    
-    # front and rear not change, and should simulate like array
-    @property
-    def front(self):
-        return self.double_linked_list.head.data
-    
-    @property
-    def rear(self):
-        return self.double_linked_list.tail.data
+
     
     # methods
+    def _next(self, pos):
+        """
+            this function return the next of position
+        """
+        pos += 1
+        if pos == self.size:
+            return 0
+        return pos
+    
+    def _prev(self, pos):
+        """
+            this function return the prev of position
+        """
+        pos -= 1
+        if pos == -1:
+            return self.size - 1
+        return pos
     def is_full(self):
         """
             this functoin return the boolean value of the deque if full or not
@@ -25,16 +35,16 @@ class Deque:
         """
         return self.added_elements == self.size
     
-    def check_full(self, func):
+    def check_full(func):
         """
             this decorator handle check full of added functions
         """
-        def wrapper():
+        def wrapper(self, value):
             # check if deque is full return no thing, else call the function
             if self.is_full():
                 print(f"cant add the deque is full")
                 return
-            func()
+            func(self, value)
             
             # increase added elements
             self.added_elements += 1
@@ -57,8 +67,10 @@ class Deque:
             returns: None
         """
         self.double_linked_list.insert_front(value)
-        
-def test1(data, size,expected):
+        self.front = self._prev(self.front)
+    def __repr__(self) -> str:
+        return str(self.double_linked_list)
+def test1(data, size, front, rear, expected):
     fun_name = inspect.currentframe().f_code.co_name
     
     print(f"{fun_name} => enque_front")
@@ -71,3 +83,14 @@ def test1(data, size,expected):
     result = str(deque)
     
     assert result == expected, f"Mismatch between expected={expected}, and result={result} in {fun_name}"
+    assert deque.front == front, f"Mismatch between expected front={front}, and result front={deque.front} in {fun_name}"
+    assert deque.rear == rear, f"Mismatch between expected rear={rear}, and result rear={deque.rear} in {fun_name}"
+
+
+if __name__ == "__main__":
+    
+    # test 1 => enque front
+    test1([1,2,3], 6, 3, 0, "[3,2,1]")
+    
+    # all tests passed
+    print("all tests passed")
