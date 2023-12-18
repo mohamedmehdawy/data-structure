b1d15fd6eb7c797095d812d906a12716f0833c41
Mohamed Mehdawy <mohammedmehdawy@gmail.com>
2023-12-18 09:03:57 +0200

    build pop function and tests passed

--- Queue/Homeworks/homework-1/implement a stack using a single queue/master.py
+++ Queue/Homeworks/homework-1/implement a stack using a single queue/master.py
@@ -13,17 +13,24 @@ class Stack:
             returns: None
         """
         # add the new element to the end
-        self.queue.enque(value)
+        self.queue.enqueue(value)
 
         # reverse the qeueu to simulate the stack
         for _ in range(len(self.queue) - 1):
-            self.queue.enque(self.queue.deque())
+            self.queue.enqueue(self.queue.dequeue())
     def peek(self):
         """
             this function remove the top element in the stack
             returns: removed element
         """
         return self.queue.front()
+
+    def pop(self):
+        """
+            this function remove the top of the stack
+            returns: the deleted element
+        """
+        return self.queue.dequeue()
     def __repr__(self) -> str:
         return str(self.queue)
     
@@ -58,6 +65,24 @@ def test2(data, expected):
     
     assert result == expected, f"Mismatch between expected={expected}, and result={result} in {fun_name}"  
 
+def test3(data, counter, expected):
+    fun_name = inspect.currentframe().f_code.co_name
+    
+    print(f"{fun_name} => pop")
+    
+    stack = Stack()
+    
+    # add elements
+    for ele in data:
+        stack.push(ele)
+
+    # pop insted of number of counter
+    for _ in range(counter):
+        stack.pop()
+    result = str(stack)
+    
+    assert result == expected, f"Mismatch between expected={expected}, and result={result} in {fun_name}"  
+
 if __name__ == "__main__":
     # test 1 => push
     test1([1,2,3], "[3,2,1]")
@@ -69,6 +94,13 @@ if __name__ == "__main__":
     test2([1,2,3], "3")
     test2([1,2], "2")
     test2([], "")
-
+    
+    # test 3 => pop
+    test3([1,2,3], 0,"[3,2,1]")
+    test3([1,2,3], 1,"[2,1]")
+    test3([1,2,3], 2,"[1]")
+    test3([1,2,3], 3,"[]")
+    test3([1,2,3], 5,"[]")
+    
     # all tests passed
     print("all tests passed")
