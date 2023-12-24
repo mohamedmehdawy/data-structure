98564464b3d5d4950e60c2370030c760952b0725
Mohamed Mehdawy <mohammedmehdawy@gmail.com>
2023-12-24 17:05:22 +0200

    build empty function and tests passed

--- Queue/Homeworks/homework-2/Problem #2 Priority Queue/master.py
+++ Queue/Homeworks/homework-2/Problem #2 Priority Queue/master.py
@@ -20,6 +20,17 @@ class PriorityQueue:
         """
 
         self.queues[str(priority)].enqueue(value)
+    
+    def empty(self):
+        """
+            this funciton return the queues if empty or not
+            returns: the status of the empty value
+        """
+        for priority in self.priorities:
+            if not self.queues[str(priority)].isEmpty():
+                return False
+        return True
+        
     def __repr__(self) -> str:
         
         
@@ -34,13 +45,28 @@ def test1(data, expected):
     
     print(f"{fun_name} => enqueue")
     
-    queue = PriorityQueue()
+    tasks = PriorityQueue()
     
     for ele in data:
-        queue.enqueue(ele[0], ele[1])
+        tasks.enqueue(ele[0], ele[1])
 
     
-    result = str(queue)
+    result = str(tasks)
+    
+    assert result == expected, f"Mismatch between expected={expected}, and result={result} in {fun_name}"  
+    
+def test2(data, expected):
+    fun_name = inspect.currentframe().f_code.co_name
+    
+    print(f"{fun_name} => empty")
+    
+    tasks = PriorityQueue()
+    
+    for ele in data:
+        tasks.enqueue(ele[0], ele[1])
+
+    
+    result = tasks.empty()
     
     assert result == expected, f"Mismatch between expected={expected}, and result={result} in {fun_name}"  
 
@@ -48,5 +74,11 @@ if __name__ == "__main__":
     # test 1 => enqueue
     test1([(1131, 1), (3111, 3), (2211, 2), (3161, 3)], "")
     
+    # test 2 => empty
+    test2([(1131, 1), (3111, 3), (2211, 2), (3161, 3)], False)
+    test2([(1131, 1), (2211, 2)], False)
+    test2([(1131, 1)], False)
+    test2([], True)
+
     # all tests passed
     print("all tests passed")
