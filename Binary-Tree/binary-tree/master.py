f6451ac9a71e2ddb087d4694b90938a441d1da1f
Mohamed Mehdawy <mohammedmehdawy@gmail.com>
2024-01-15 14:57:30 +0200

    create 8 nodes, and link them non linear

--- Binary-Tree/binary-tree/master.py
+++ Binary-Tree/binary-tree/master.py
@@ -1,10 +1,36 @@
 class Node:
-    def __init__(self, data=None) -> None:
+    def __init__(self, data=None, left=None, right=None) -> None:
         self.data = data
-        self.left = None
-        self.right = None
+        self.left = left
+        self.right = right
 
 
 class BinaryTree:
     def __init__(self) -> None:
         self.root = None
+
+
+if __name__ == "__main__":
+    # create 8 nodes
+    root = Node(1)
+    node2 = Node(2)
+    node3 = Node(3)
+    node4 = Node(4)
+    node5 = Node(5)
+    node6 = Node(6)
+    node7 = Node(7)
+    node8 = Node(8)
+
+    # link them together
+
+    root.left = node2
+    root.right = node3
+
+    node2.left = node4
+    node2.right = node5
+
+    node5.right = node7
+
+    node3.right = node6
+
+    node6.left = node8
