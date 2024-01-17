abc1d74067be7156f243657461cf96d41cdcd5cc
Mohamed Mehdawy <mohammedmehdawy@gmail.com>
2024-01-17 08:25:40 +0200

    build print expression and tests passed

--- Binary-Tree/binary-tree/master.py
+++ Binary-Tree/binary-tree/master.py
@@ -35,21 +35,54 @@ def print_expression(root):
         parameters:
             root: the root will start print the left and right from it
     """
-    pass
+    # check if the root is operator
+    if not str(root.data).isdigit():
+        left_side = print_expression(root.left)
+        right_side = print_expression(root.right)
+
+        return f"{left_side}{right_side}{root.data}"
+
+    # if the root is a number, just return the value
+    return root.data
 
 
 if __name__ == "__main__":
-    # create 8 nodes
-    root = Node(1)
-    node2 = Node(2)
-    node3 = Node(3)
-    node4 = Node(4)
-    node5 = Node(5)
-    node6 = Node(6)
-    node7 = Node(7)
-    node8 = Node(8)
+    # # create 8 nodes
+    # root = Node(1)
+    # node2 = Node(2)
+    # node3 = Node(3)
+    # node4 = Node(4)
+    # node5 = Node(5)
+    # node6 = Node(6)
+    # node7 = Node(7)
+    # node8 = Node(8)
 
-    # link them together
+    # # link them together
+
+    # root.left = node2
+    # root.right = node3
+
+    # node2.left = node4
+    # node2.right = node5
+
+    # node5.right = node7
+
+    # node3.right = node6
+
+    # node6.left = node8
+
+    # print_tree(root)
+
+    # test for print expression
+    root = Node("*")
+
+    node2 = Node("+")
+    node3 = Node(4)
+
+    node4 = Node(2)
+    node5 = Node(3)
+
+    # link nodes
 
     root.left = node2
     root.right = node3
@@ -57,10 +90,4 @@ if __name__ == "__main__":
     node2.left = node4
     node2.right = node5
 
-    node5.right = node7
-
-    node3.right = node6
-
-    node6.left = node8
-
-    print_tree(root)
+    print(print_expression(root))
