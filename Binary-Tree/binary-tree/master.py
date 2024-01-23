0c99af42ad9f039bf4333265c7a6f72ea1c5a1de
Mohamed Mehdawy <mohammedmehdawy@gmail.com>
2024-01-23 09:50:57 +0200

    build inorder function and tests passed

--- Binary-Tree/binary-tree/master.py
+++ Binary-Tree/binary-tree/master.py
@@ -9,6 +9,30 @@ class BinaryTree:
     def __init__(self) -> None:
         self.root = None
 
+    def print_inorder(self):
+        """
+            this function print the tree in inorder way
+        """
+        def inorder(current):
+            """
+                this function print the tree starts from the current
+                parameters:
+                    current: the starts root point
+            """
+            # if arrive to the last of the sub tree stop the recursion
+            if not current:
+                return
+
+            # call for left side
+            inorder(current.left)
+
+            # print the current node
+            print(current.data, end="")
+
+            # call for right side
+            inorder(current.right)
+        inorder(self.root)
+
 
 def print_tree(root):
     """
@@ -62,42 +86,17 @@ def print_expression_postfix(root):
 
 
 if __name__ == "__main__":
-    # # create 8 nodes
-    # root = Node(1)
-    # node2 = Node(2)
-    # node3 = Node(3)
-    # node4 = Node(4)
-    # node5 = Node(5)
-    # node6 = Node(6)
-    # node7 = Node(7)
-    # node8 = Node(8)
+    # create 8 nodes
+    root = Node(1)
+    node2 = Node(2)
+    node3 = Node(3)
+    node4 = Node(4)
+    node5 = Node(5)
+    node6 = Node(6)
+    node7 = Node(7)
+    node8 = Node(8)
 
-    # # link them together
-
-    # root.left = node2
-    # root.right = node3
-
-    # node2.left = node4
-    # node2.right = node5
-
-    # node5.right = node7
-
-    # node3.right = node6
-
-    # node6.left = node8
-
-    # print_tree(root)
-
-    # test for print expression
-    root = Node("*")
-
-    node2 = Node("+")
-    node3 = Node(4)
-
-    node4 = Node(2)
-    node5 = Node(3)
-
-    # link nodes
+    # link them together
 
     root.left = node2
     root.right = node3
@@ -105,5 +104,33 @@ if __name__ == "__main__":
     node2.left = node4
     node2.right = node5
 
-    print(print_expression(root))
-    print_expression_postfix(root)
+    node5.right = node7
+
+    node3.right = node6
+
+    node6.left = node8
+
+    print_tree(root)
+
+    tree = BinaryTree()
+    tree.root = root
+    tree.print_inorder()
+    # test for print expression
+    # root = Node("*")
+
+    # node2 = Node("+")
+    # node3 = Node(4)
+
+    # node4 = Node(2)
+    # node5 = Node(3)
+
+    # # link nodes
+
+    # root.left = node2
+    # root.right = node3
+
+    # node2.left = node4
+    # node2.right = node5
+
+    # print(print_expression(root))
+    # print_expression_postfix(root)
