1cac1ff1088570dbaa1dd43e29365a2892001021
Mohamed Mehdawy <mohammedmehdawy@gmail.com>
2024-02-09 19:50:46 +0200

    solve the problem and passed test cases

--- Binary-Tree/Homeworks/Problem #3 LeetCode 404 - Sum of Left Leaves/master.py
+++ Binary-Tree/Homeworks/Problem #3 LeetCode 404 - Sum of Left Leaves/master.py
@@ -6,4 +6,21 @@
 #         self.right = right
 class Solution:
     def sumOfLeftLeaves(self, root: Optional[TreeNode]) -> int:
-        pass
+        # if not root return 0
+        if not root:
+            return 0
+
+        # call same function for left and right to arraive to the heighst node
+        left_side = self.sumOfLeftLeaves(root.left)
+        right_side = self.sumOfLeftLeaves(root.right)
+
+        # sum the values of left and right
+        sum_sides = left_side + right_side
+
+        # if the current node has left node, and the left node is leaf add it so the sum_sides
+
+        if root.left and not root.left.left and not root.left.right:
+            return sum_sides + root.left.value
+
+        # return the sum
+        return sum_sides
