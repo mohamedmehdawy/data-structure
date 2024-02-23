89d8e7caf7627865bf66bb96213fda4ea4f31c72
Mohamed Mehdawy <mohammedmehdawy@gmail.com>
2024-02-23 14:24:54 +0200

    update base binary tree

--- Binary-Tree/binary-tree/master.py
+++ Binary-Tree/binary-tree/master.py
@@ -8,6 +8,8 @@ class Node:
 class BinaryTree:
     def __init__(self, root) -> None:
         self.root = Node(root)
+        # length will start with 1, because we already add the root node
+        self.length = 1
 
     def add(self, nodes, directions):
         """
@@ -18,13 +20,11 @@ class BinaryTree:
         """
         # init current
         current = self.root
-
         # loop insted of the index
         for index in range(len(nodes)):
             current = self.add_node(current, nodes[index], directions[index])
 
-    @staticmethod
-    def add_node(current, value, direction):
+    def add_node(self, current, value, direction):
         """
             this function link the node insted of his direction
             parameters:
@@ -32,13 +32,18 @@ class BinaryTree:
                 value: the value of current or new node
                 direction: the direction of the linked (left or right)
         """
+        # increase the length
         # if the direction is left
         if direction == "L":
             if not current.left:
                 # create new node and link with current.left
                 current.left = Node(value)
+                self.length += 1
+
             else:
                 assert current.left.data == value
+                self.length += 1
+
             return current.left
 
         # if the direction is right
@@ -73,7 +78,54 @@ class BinaryTree:
             inorder(current.right)
         inorder(self.root)
 
-
+    def first_not_perfect_check(self):
+        """
+            this function handle first check for the tree if no have any node or 1 node
+            returns:
+                true if the tree is not perfect
+        """
+        return self.length <= 1
+    def _tree_height(self, root):
+        """
+            this function return the height of the sub tree
+            parameters:
+                root: the root you will start get the height from it
+                height: the height of the subtree
+            returns: the height of the sub tree
+        """
+        # if no root, return -1
+        if not root:
+            return -1
+        
+        # return the max of left and right
+        return 1 + max(self._tree_height(root.left), self._tree_height(root.right))
+    
+    def tree_height(self):
+        """
+            this function return the height of the tree
+            returns: the height of the tree
+        """
+        return self._tree_height(self.root)
+    def _tree_nodes(self, current):
+        """
+            this function return the total number of nodes
+            parameters:
+                current: the current node
+            returns: total number of tree nodes
+        """
+        # if no current return 0
+        if not current:
+            return 0
+        
+        return 1 + self._tree_nodes(current.left) + self._tree_nodes(current.right)
+    def tree_nodes(self):
+        """
+            this function return the total number of nodes
+            returns: total number of tree nodes
+        """
+        
+        return self._tree_nodes(self.root)
+    
 def print_tree(root):
     """
         this function the binary tree recursively
