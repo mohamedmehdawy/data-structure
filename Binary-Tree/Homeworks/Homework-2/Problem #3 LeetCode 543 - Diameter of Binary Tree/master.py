3723eb80952ebf65bee086b36a0322eb02aea164
Mohamed Mehdawy <mohammedmehdawy@gmail.com>
2024-03-04 14:32:34 +0200

    write the algorithm and init the code for  Diameter of Binary Tree

---
+++ Binary-Tree/Homeworks/Homework-2/Problem #3 LeetCode 543 - Diameter of Binary Tree/master.py
@@ -0,0 +1,27 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def _tree_height(self, root):
+        """
+            this function return the height of the sub tree
+            parameters:
+                root: the root you will start get the height from it
+                height: the height of the subtree
+            returns: the height of the sub tree
+        """
+        # if no root, return -1
+        if not root:
+            return -1
+        
+        # return the max of left and right
+        return 1 + max(self._tree_height(root.left), self._tree_height(root.right))
+    
+    def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
+        """
+            this function return the diameter of a binary tree
+            returns: the length of the longest path between any two nodes in a tree
+        """
